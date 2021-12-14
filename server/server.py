1314edd064a5b8299fa0322dc4dce94e44cbb156
Geronimo <rafaelgeronimofx@gmail.com>
2021-12-14 15:42:03 -0300

    iniciando a cosntrução do server

--- server/server.py
+++ server/server.py
@@ -16,3 +16,21 @@ def main():
     while True:
         client, addr = server.accept()
         clients.append(client)
+
+def messagesTreatment(client):
+    while True:
+        try:
+            messages = client.recv(1024)
+        except:
+            pass
+
+def broadcast(messages, client):
+    for clientItem in clients:
+        if clientItem != client:
+            try:
+                clientItem.send(messages)
+            except:
+                deleteClient(clientItem)
+
+def deleteClient(client):
+    client.remove(client)
